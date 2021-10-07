ab77010f0e7ad430d117d5a34eaa8d399b6bac4a
Israel Olorunsola <olorunsolaisrael353@gmail.com>
2021-10-07 23:16:04 +0100

    fix/deploy errors

--- src/pages/Career.jsx
+++ src/pages/Career.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import PageHeader from "../components/PageHeader"
 import IconCard from "../components/CareersIconCard"
 import { JoinUsButton } from "../components/Button"
